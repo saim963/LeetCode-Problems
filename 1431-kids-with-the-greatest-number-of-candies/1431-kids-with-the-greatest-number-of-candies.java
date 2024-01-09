e675c1cb07ab7adf45a41fd442a70460107b5657
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-10 02:57:08 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (7.21%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -2,18 +2,27 @@ class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         //need to create a Boolean[] responseArr;
         List<Boolean> response = new ArrayList<>();
-        Boolean isGreatest ;
+        // Boolean isGreatest ;
+        // for(int i=0; i<candies.length; i++){
+        //     candies[i] += extraCandies;
+        //     isGreatest = true;
+        //     for(int j=0; j<candies.length; j++){
+        //         if(candies[i]<candies[j]){
+        //             isGreatest = false;
+        //             break;
+        //         }
+        //     }
+            // response.add(isGreatest);
+            // candies[i] -= extraCandies;
+        // }
+        int maxCandies = candies[0];
         for(int i=0; i<candies.length; i++){
-            candies[i] += extraCandies;
-            isGreatest = true;
-            for(int j=0; j<candies.length; j++){
-                if(candies[i]<candies[j]){
-                    isGreatest = false;
-                    break;
-                }
+            if(candies[i]>maxCandies){
+                maxCandies = candies[i];
             }
-            response.add(isGreatest);
-            candies[i] -= extraCandies;
+        }
+        for(int j=0; j<candies.length; j++){
+            response.add(candies[j]+extraCandies>=maxCandies);
         }
         return response;
     }
