cfd606d507a1c39060b771f8e2c23fe88bf71c13
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-13 23:15:24 +0530

    Time: 255 ms (38.89%), Space: 39 MB (61.73%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.kt
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun buildArray(nums: IntArray): IntArray {
-        val ans:IntArray = IntArray(nums.size) 
+        val ans = IntArray(nums.size) 
         for(i in 0 until nums.size){
             ans[i] = nums[nums[i]]
         }
