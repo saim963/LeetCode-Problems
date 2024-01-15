b568e5d6cc584759854e133361bf8da05055221d
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-16 04:46:53 +0530

    Time: 4 ms (34.04%), Space: 41.8 MB (8.52%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        /*String checker = "abcdefghijklmnopqrstuvwxyz";
-        for(int i = 0; i<sentence.length(); i++){
-            if(sentence.indexOf(checker.charAt(i)) == -1)//problem is the order here
-                //and this does not handles the duplicate issue 
-                return false;
-        }
-        return true;*/
-        //will be using set here
         Set<Character> charSet = new HashSet<>();
         for(int i=0; i<sentence.length(); i++){
             charSet.add(sentence.charAt(i));
