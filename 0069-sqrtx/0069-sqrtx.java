8e302a30204c9797be38d5042dfae27f02b5c7ce
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-20 23:19:54 +0530

    Time: 19 ms (11.96%), Space: 41.3 MB (7.19%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+    int count = 0;
+    int odd = 1;
+        while(x>0){
+            x = x-odd;
+            if(x<0)
+                return count;
+            odd+=2;
+            count++;
+        }
+        return  count;
     }
 }
