397112ae0e2959a15fcb5235bc9da3c5e1150a13
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-30 01:02:01 +0530

    Time: 1 ms (15.58%), Space: 56.3 MB (42.96%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,11 +1,20 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        //the brute force method
+        /*
+        //the brute force method -> linear search
         int max = 0;
         for(int i=0; i<arr.length; i++){
             if(arr[i]>arr[max])
                 max = i;
         }
         return max;
+        */
+        
+        //One more simple solution but not optimised
+        int i=0;
+        while(arr[i]<arr[i+1])
+            i++;
+        return i;
+        
     }
 }
