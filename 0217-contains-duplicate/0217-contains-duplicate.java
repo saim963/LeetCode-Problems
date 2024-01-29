9e9562e0d553ad1fdf0d138c1beeca99fdaca19e
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-29 14:37:05 +0530

    Time: 10 ms (87.73%), Space: 60.6 MB (15.50%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+    /*
         Arrays.sort(nums);
         int count = 0;
         for(int i=0 ; i<nums.length-1; i++){
@@ -7,5 +8,15 @@ class Solution {
                 count++;
         }
         return count == 0 ? false : true;
+    */
+        //using hashset
+        HashSet<Integer> set = new HashSet<>();
+        for(int element: nums){
+            if(set.contains(element))
+                return true;
+            set.add(element);
+        }
+        return false;
     }
+    
 }
