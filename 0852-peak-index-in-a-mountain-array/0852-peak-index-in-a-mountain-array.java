882a8cb5af1b78235355af2de9a4ce90109b772c
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-01-30 12:52:49 +0530

    Time: 0 ms (100.00%), Space: 56.4 MB (34.55%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -9,12 +9,22 @@ class Solution {
         }
         return max;
         */
-        
+        /*
         //One more simple solution but not optimised
         int i=0;
         while(arr[i]<arr[i+1])
             i++;
         return i;
-        
+        */
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1])
+                end = mid;
+            else
+                start = mid + 1;
+        }
+        return start;
     }
 }
