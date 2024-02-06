9749126fd904df9bffa0d13dabd900feb479287c
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-06 15:25:55 +0530

    Time: 17 ms (11.75%), Space: 40.8 MB (51.64%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -3,9 +3,9 @@ class Solution {
     int count = 0;
     int odd = 1;
         while(x>0){
-            x = x-odd;
+            x = x-odd;//heres one method to find the sqrt in which we reduce odds till 0
             if(x<0)
-                return count;
+                return count;//no of times we needed to redducee odd till 0
             odd+=2;
             count++;
         }
