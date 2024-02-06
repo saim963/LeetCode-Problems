23e76eda473c0e45c68b9fe18a2997c23dd328a9
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-06 15:11:02 +0530

    Time: 2 ms (92.61%), Space: 46.9 MB (56.40%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,12 +4,13 @@ class Solution {
         int start=0;
         int end=numbers.length-1;
         while(start<=end){
-            int sum = numbers[start] + numbers[end];//17,13
-            if(sum > target){//T
-                end--;//e=2
-            }else if(sum < target){
+            int sum = numbers[start] + numbers[end];
+            //here instead of mid element i used sum to compare
+            if(sum > target){//if sum is greater than target we we goto starting side
+                end--;
+            }else if(sum < target){//else we got to endinng side
                 start++;
-            }else{
+            }else{//here we acknowledge equilibrium and prints ans in 1s index
                 result[0] = start+1;
                 result[1] = end+1;
                 return result;
