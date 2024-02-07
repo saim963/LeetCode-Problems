3e9da8e9cadf851c2e93d4b8ec2732dc07271552
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-07 16:18:34 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (8.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,10 +4,8 @@ class Solution {
         int end = nums.length - 1;
         while(start<end){
             int mid = start + (end-start)/2;
-            if(nums[mid]>nums[mid+1])
-                end = mid;
-            else
-                start = mid + 1;
+            if(nums[mid]>nums[mid+1])   end = mid;
+            else    start = mid + 1;
         }
         return start;
     }
