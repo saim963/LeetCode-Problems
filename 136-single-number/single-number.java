1a642bb614f04877db8be2d130b981a87443ff0a
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-10 01:48:02 +0530

    Time: 1 ms (99.87%) | Memory: 46.5 MB (7.20%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //XOR operator cancles out same numbers
+        //XOR operator cancels out same numbers
         int result = 0;
         for (int num : nums) {
             result ^= num;
