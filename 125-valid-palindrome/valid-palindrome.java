46c9f6562861622e9fee42ce69df54ad262f2b88
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-14 16:52:05 +0530

    Time: 5 ms (51.73%) | Memory: 43.4 MB (53.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,14 +2,12 @@ class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder builder = new StringBuilder();
         String x =s.toLowerCase();//every element is in lowercase
-        System.out.println(x);
         //non-alphabetical characters
         for(int i=0; i<x.length(); i++){
             if(x.charAt(i)>=(char)48 && x.charAt(i)<=(char)57 || x.charAt(i)>=(char)97 && x.charAt(i)<=(char)122){
                 builder.append(x.charAt(i));
             }
         }
-        System.out.println(builder);
         return builder.toString().contentEquals(builder.reverse());
     }
 }
