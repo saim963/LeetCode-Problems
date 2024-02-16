a9df263df4017ba65776b154a8b8b704842e7e76
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-16 22:53:22 +0530

    Time: 6 ms (38.32%) | Memory: 44.7 MB (46.15%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        //We'll move from L to R if element is lesser we reduce else all added
+        //e.g. MCMXCIV -> 5-1+100-10+1000-100+1000 -> 1994
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int result = map.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2; i>=0; i--){
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1)))
+                result -= map.get(s.charAt(i));
+            else
+                result += map.get(s.charAt(i));
+        }
+        return result;
+    }
+}
