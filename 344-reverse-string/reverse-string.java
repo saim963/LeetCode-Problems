1c7d9690b6a2bd20dfb6295e46d5c8179e997f8b
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-18 02:45:54 +0530

    Time: 1 ms (33.55%) | Memory: 49.5 MB (21.29%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void reverseString(char[] s) {
+        /*
+         * int j=0;
+         * int l=s.length;
+         * char[] r = new char[l];
+         * for(int i=l-1; i>=0; i--){
+         * r[j]=s[i];
+         * j++;
+         * }//Problem with this code is SC should e O(1);
+         */
+        // reverse by swapping or using collection.reverse() method or two pointers
+        char temp;
+        for(int i=0; i<s.length/2; i++){
+            temp = s[i];
+            s[i]=s[s.length-i-1];
+            s[s.length-i-1]=temp;
+        }
+    }
+}
