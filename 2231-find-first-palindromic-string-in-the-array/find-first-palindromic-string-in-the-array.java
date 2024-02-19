06bf2b3e1f7576e1c0d1f45b2375c67daac97b79
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-19 23:22:45 +0530

    Time: 32 ms (5.39%) | Memory: 44.9 MB (65.22%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(String element: words){
+            if(isPalindrome(element))   return element;
+        }
+        return "";
+    }
+    boolean isPalindrome(String s){
+        StringBuilder builder = new StringBuilder();
+        String x =s.toLowerCase();//every element is in lowercase
+        //non-alphabetical characters
+        for(int i=0; i<x.length(); i++){
+            if(x.charAt(i)>=(char)48 && x.charAt(i)<=(char)57 || x.charAt(i)>=(char)97 && x.charAt(i)<=(char)122){
+                builder.append(x.charAt(i));
+            }
+        }
+        return builder.toString().contentEquals(builder.reverse());
+    }
+}
