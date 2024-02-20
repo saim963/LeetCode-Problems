06aea5f71b585bf6032fd5c1e7921d002a055cf3
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-20 15:58:48 +0530

    Time: 2 ms (95.02%) | Memory: 43.3 MB (39.31%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
         // means return common subarray
-        HashSet<Integer> set1 = new HashSet<>();// 1 2
-        HashSet<Integer> intersectSet = new HashSet<>(set1);
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> intersectSet = new HashSet<>();
         for (int num1 : nums1)      set1.add(num1);
         for (int num2 : nums2) {
             if(set1.contains(num2))     intersectSet.add(num2);
