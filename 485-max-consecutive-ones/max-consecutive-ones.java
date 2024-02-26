b7cf8b97d71c8389fc07f856a1cb4ade4a12010e
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-26 20:45:46 +0530

    Time: 2 ms (90.62%) | Memory: 45.5 MB (48.91%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             if(nums[i]==1)    count++;
             else   count=0;
-            max=Math.max(max,count);
+            if(count>=max)   max=count;
+            //using math max function increase TC but optimizes SC
         }
         return max;
     }
