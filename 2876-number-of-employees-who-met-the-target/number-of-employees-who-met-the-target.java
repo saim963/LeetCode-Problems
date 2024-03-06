33c1a1e8d3d95e10a3abe7b63ccf91e83e311d16
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-03-06 11:17:56 +0530

    Time: 5 ms (22.92%) | Memory: 42.5 MB (18.34%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -1,7 +1,11 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int count=0 ;
-        for(int num: hours) if(num>=target) count++;
-        return count;
+        // int count=0 ;
+        // for(int num: hours) if(num>=target) count++;
+        // return count;
+
+
+        //More optimized
+        return (int) IntStream.of(hours).filter(hour -> hour >= target).count();
     }
 }
