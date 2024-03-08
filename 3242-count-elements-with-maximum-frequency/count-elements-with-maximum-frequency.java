7d3443325c19dda7ece0555462f5bf9e8a0a06f5
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-03-09 02:21:36 +0530

    Time: 1 ms (99.63%) | Memory: 42.3 MB (52.25%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int[] arr = new int[101];//here the constraints are small, but maps should be used for bigger constraints
+        int maxF = 0;
+        for(int ele: nums){
+            arr[ele]++;
+            maxF = Math.max(maxF,arr[ele]);
+        }
+        int result =0;
+        for(int ele: arr){
+            if(ele==maxF){
+                result +=maxF ;
+            }   
+        }
+        return result;
+    }
+
+}
