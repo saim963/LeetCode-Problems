3dcea2869128dd62449512d238299ee31f6d4118
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-03-18 23:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (47.80%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,5 +1,6 @@
 class Solution {
     public int pivotInteger(int n) {
+        //was kind off logical
         int sum = n*(n+1)/2;
         int pivot = (int)Math.pow(sum,0.5);
         return (pivot*pivot) == sum ? pivot:-1;
