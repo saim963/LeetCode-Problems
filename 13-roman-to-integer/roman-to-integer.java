4ea6cb4e41fdfed5a5b3f9a8a165fcd21f75b862
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-03-27 22:27:06 +0530

    Time: 2 ms (100.00%) | Memory: 44.3 MB (88.92%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,31 +1,32 @@
 class Solution {
     public int romanToInt(String s) {
-        //We'll move from L to R if element is lesser we reduce else all added
-        //e.g. MCMXCIV -> 5-1+100-10+1000-100+1000 -> 1994
+        // We'll move from L to R if element is lesser we reduce else all added
+        // e.g. MCMXCIV -> 5-1+100-10+1000-100+1000 -> 1994
         /*
-        HashMap<Character,Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+         * HashMap<Character,Integer> map = new HashMap<>();
+         * map.put('I',1);
+         * map.put('V',5);
+         * map.put('X',10);
+         * map.put('L',50);
+         * map.put('C',100);
+         * map.put('D',500);
+         * map.put('M',1000);
+         * 
+         * int result = map.get(s.charAt(s.length()-1));
+         * for(int i=s.length()-2; i>=0; i--){
+         * if(map.get(s.charAt(i))<map.get(s.charAt(i+1)))
+         * result -= map.get(s.charAt(i));
+         * else
+         * result += map.get(s.charAt(i));
+         * }
+         * return result;
+         */
 
-        int result = map.get(s.charAt(s.length()-1));
-        for(int i=s.length()-2; i>=0; i--){
-            if(map.get(s.charAt(i))<map.get(s.charAt(i+1)))
-                result -= map.get(s.charAt(i));
-            else
-                result += map.get(s.charAt(i));
-        }
-        return result;
-        */
-
-        //upper approach is of mapping and this lower approach uses only string and performs better
-        int value=0, prev=0, cur=0;
-        for(int i=s.length()-1; i>=0; i--){
-            cur = switch(s.charAt(i)){
+        // upper approach is of mapping and this lower approach uses only string and
+        // performs better
+        int value = 0, prev = 0, cur = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            cur = switch (s.charAt(i)) {
                 case 'M' -> 1000;
                 case 'D' -> 500;
                 case 'C' -> 100;
@@ -34,9 +35,11 @@ class Solution {
                 case 'V' -> 5;
                 default -> 1;
             };
-            if(cur<prev)    value-=cur;
-            else    value+=cur;
-            prev=cur;
+            if (cur < prev)
+                value -= cur;
+            else
+                value += cur;
+            prev = cur;
         }
         return value;
     }
