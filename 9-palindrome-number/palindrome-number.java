4b1c9fe605abce3e29637cf366e7da6d517b2c01
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-04-10 01:50:07 +0530

    Time: 5 ms (75.21%) | Memory: 43.9 MB (60.41%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,14 +3,14 @@ class Solution {
         if(x<0) return false;
         return x==reverse(x);
     }
-    public int reverse(int n){
-        int rev = 0;
+    public int reverse(int x) {
+        int rev=0;
         int min = Integer.MIN_VALUE/10;
         int max = Integer.MAX_VALUE/10;
-        while(n!=0){
+        while(x!=0){
             if(rev>max || rev<min)  return 0;
-            rev = rev*10 + n%10;
-            n/=10;
+            rev = rev*10 + x%10;
+            x/=10;
         }
         return rev;
     }
