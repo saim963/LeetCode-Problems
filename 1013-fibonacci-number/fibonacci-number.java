3e3a4bceef62d4ca86eb550a6a0d4cd53c82f070
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-04-11 02:16:05 +0530

    Time: 9 ms (26.27%) | Memory: 40 MB (84.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)    return 0;
-        else if(n==1)   return 1;
+        if(n==0 || n==1)    return n;
         return fib(n-1)+fib(n-2);
     }
     //Recursion will always be worse or equal to an iterative approach both time- and memory-wise
