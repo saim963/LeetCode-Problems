aa5d262563d5c1ab73f1e3d658088faa627260f2
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-04-17 01:02:54 +0530

    Time: 2 ms (79.06%) | Memory: 61.6 MB (46.84%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,40 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //Brute Force  here time limit is exceeding
-        // int max = 0;
-        // for(int i=0; i<prices.length; i++){
-        //     for(int j =i+1; j<prices.length; j++){
-        //         if(prices[j]>prices[i]){
-        //             int currPro = prices[j]-prices[i];
-        //             max = Math.max(currPro,max);
-        //         }
-        //     }
-        // }
-        // return max;
-
-        //This can still be optimied
-        // int[] nums = new int[prices.length-1];
-        // for(int i=1; i<prices.length; i++){
-        //     nums[i-1] = prices[i]-prices[i-1]; 
-        // }
-        // //here difference array is created now kadane's algo
-        // //three step process sum+=arr[i], maxi(maxi,sum), if(sum<0)sum=0;
-        // int sum =0;
-        // int maxSum =0;
-        // for(int i=0; i<nums.length; i++){
-        //     sum += nums[i];
-        //     maxSum = Math.max(maxSum,sum);
-        //     if(sum<0)   sum=0;
-        // }
-        // return maxSum;
-
-        //final hit
-        int maxPro = 0;
-        int currPro = 0;
+        int currPro =0;
+        int maxPro =0;
         for(int i=1; i<prices.length; i++){
             currPro += prices[i]-prices[i-1];
             if(currPro<0)   currPro=0;
-            maxPro = Math.max(currPro,maxPro); 
+            maxPro = Math.max(currPro,maxPro);
         }
         return maxPro;
     }
