c771b5787d33687d00c7e82e6d4f302e2c01e40d
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-04-22 02:11:43 +0530

    Time: 4 ms (89.35%) | Memory: 44.7 MB (31.24%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int count =0;
+        StringBuilder sb = new StringBuilder();
+        for(char ch: s.toCharArray()){
+            int index = sb.indexOf(String.valueOf(ch));
+            if(index != -1){
+                count = Math.max(sb.length(),count);
+                sb.delete(0, index + 1);
+            }
+            sb.append(ch);
+        }
+        count = Math.max(sb.length(),count);
+        return count;
+    }
+}
