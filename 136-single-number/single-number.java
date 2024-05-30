3e7861a89c6bd2e749efc64cfaa39c17549d023f
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-05-31 02:11:08 +0530

    Time: 13 ms (19.55%) | Memory: 44.7 MB (95.23%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,10 +1,20 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //XOR operator cancels out same numbers
-        int result = 0;
-        for (int num : nums) {
+        //make hashmap and return the element whose count is one
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int el: nums){
+            map.put(el,map.getOrDefault(el,0)+1);
+        }
+        for(int key: map.keySet()){
+            if(map.get(key)==1) return key;
+        }
+        return -1;
+
+        //using XOR operator
+        /*int result = 0;
+        for(int num: nums){
             result ^= num;
         }
-        return result;
+        return result;*/
     }
 }
