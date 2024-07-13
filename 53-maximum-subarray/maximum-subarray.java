dfa964e49a81258779371cfeb6f1d785febb9615
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-07-13 21:54:07 +0530

    Time: 1 ms (99.63%) | Memory: 57.6 MB (7.01%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //three step process sum+=arr[i], maxi(maxi,sum), if(sum<0)sum=0;
-        int sum =0;
+    //three step process sum+=arr[i], maxi(maxi,sum), if(sum<0)sum=0;
+        int currSum =0;
         int maxSum =nums[0];
-        for(int num: nums){
-            sum +=num;
-            maxSum =Math.max(maxSum,sum);
-            if(sum<0)   sum=0;
+        for(int el: nums){
+            currSum += el;
+            maxSum = Math.max(currSum,maxSum);
+            if(currSum<0)   currSum =0;
         }
         return maxSum;
     }
