a2351d394e4261475009a0dd42b889d682292ea1
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-08-19 14:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (76.98%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,15 +1,14 @@
 class Solution {
     public int minSteps(int n) {
-        int count =0;
-        int d =2;
+        int result =0;
+        int factor =2;
         while(n>1){
-            //if d ia prime factor add to ans and keep diving else increase d
-            while(n%d == 0){
-                count += d;
-                n /= d;
+            while(n%factor == 0){
+                result += factor;
+                n/=factor;
             }
-            d++;
+            factor++;  
         }
-        return count;
+        return result;
     }
 }
