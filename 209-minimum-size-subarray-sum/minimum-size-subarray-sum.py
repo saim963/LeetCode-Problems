bc2bb64f6a8a2d759fbaebad35115b4b7fd0d984
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2025-09-01 19:00:26 +0530

    Time: 8 ms (66.06%) | Memory: 28.3 MB (36.46%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,11 +1,21 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        #defining l-pointer and cur-current sum
         l,cur = 0,0
+        #minimum length is initiatedd with maximum possible number
         min_len = float('inf')
+        #Looping through the list this is how we increase size of sliding window
         for r in range(len(nums)):
+            #While r is moving forward the current sum is incrementing
             cur += nums[r]
+            #In case current sum exceeds the target value 
             while cur >= target:
-                min_len = min(min_len, r-l+1)
+                #At that very moment we captures the length of the subarray
+                min_len = min(min_len,r-l+1)
+                #Now reducing the value of subarray by reducing the left pointer element
                 cur -= nums[l]
+                #moving the left pointer forward
                 l += 1
-        return min_len if min_len < float('inf') else 0
+        #returning the minimum lenght of the subarray, incase the min_value was not upgraded
+        #In case the array didn't have any subarray to sum till target we return 0
+        return min_len if min_len!=float('inf') else 0
