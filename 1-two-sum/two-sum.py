84603823fbcb5098b2f285a9e7cdaf766dbf3fb8
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2025-09-11 18:43:44 +0530

    Time: 2 ms (53.01%) | Memory: 19.1 MB (23.68%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #Dictionray to store seen numbers and their indexes
         num_dic = {}
+        #Enumerating through the nums list
         for i,num in enumerate(nums):
+            #calculating diff for each num
             diff = target - num
+            #if diff is in num dictionary 
             if diff in num_dic:
+                #we call the index of the diff and current index
                 return [num_dic[diff],i]
+            #else we store the current number with index
             num_dic[num] = i
+        #In case no solution found
         return []
